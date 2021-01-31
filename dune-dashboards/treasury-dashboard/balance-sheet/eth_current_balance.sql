58d88ed7837bf095a5a66d7bc20156939d278b96
Thomas Hepner <hepner.thomas@gmail.com>
2021-01-31 09:10:27 -0800

    Treasury and Tokenomics Dashboard Queries
    Added USD prices and balance to ETH balance query.

--- dune-dashboards/treasury-dashboard/balance-sheet/eth_current_balance.sql
+++ dune-dashboards/treasury-dashboard/balance-sheet/eth_current_balance.sql
@@ -1,14 +1,7 @@
-/* 
-    Etherscan URL: https://etherscan.io/address/0x9467cfadc9de245010df95ec6a585a506a8ad5fc
-
-*/
-
-
-SELECT address, sum(amount) / 1e18 as amount
-FROM (
+WITH eth_balance AS ( 
 
     -- outbound transfers
-    SELECT "from" AS address, -tr.value AS amount
+    SELECT "from" AS address, -tr.value AS amount, 'WETH' as "symbol"
     FROM ethereum.traces tr
     WHERE "from" = '\x9467cfadc9de245010df95ec6a585a506a8ad5fc'
     AND success
@@ -18,7 +11,7 @@ FROM (
     UNION ALL
 
     -- inbound transfers
-    SELECT "to" AS address, value AS amount
+    SELECT "to" AS address, value AS amount, 'WETH' as "symbol"
     FROM ethereum.traces
     WHERE "to" = '\x9467cfadc9de245010df95ec6a585a506a8ad5fc'
     AND success
@@ -27,9 +20,57 @@ FROM (
     UNION ALL
     
     -- gas costs
-    SELECT "from" AS address, -gas_used * gas_price AS amount
+    SELECT "from" AS address, -gas_used * gas_price AS amount, 'WETH' as "symbol"
     FROM ethereum.transactions
     WHERE "from" = '\x9467cfadc9de245010df95ec6a585a506a8ad5fc'
+
+) 
+
+
+
+, most_recent_prices AS (
+
+    SELECT 
+        symbol, 
+        contract_address,
+        price
     
-) t
-GROUP BY 1
+    FROM (
+
+        SELECT 
+            symbol, 
+            contract_address,
+            price,
+            minute,
+            ROW_NUMBER() OVER (PARTITION BY contract_address ORDER BY minute DESC) AS RowNumber
+            
+        FROM prices.usd 
+        
+        WHERE symbol IN ('WETH') 
+    
+    ) asset_prices    
+
+    WHERE RowNumber = 1
+
+)
+
+
+/* --- Main Query --- */
+SELECT 
+    'Index Token Treasury' as wallet, 
+    address as wallet_address,
+    mrp.symbol,
+    mrp.contract_address,
+    sum(amount) / 1e18 as units,
+    price,
+    sum(amount) / 1e18 * price as balance
+
+FROM eth_balance eb 
+
+LEFT JOIN most_recent_prices mrp ON eb.symbol = mrp.symbol
+
+GROUP BY 
+    address,
+    mrp.symbol,
+    mrp.price,
+    mrp.contract_address
