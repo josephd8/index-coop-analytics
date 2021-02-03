503dcdc5f8f1a17821d71adddd32f27edc004d32
Thomas Hepner <hepner.thomas@gmail.com>
2021-02-03 13:05:18 -0800

    Add Treasury Committee and Community Treasury 1 Year Vesting wallets

--- dune-dashboards/treasury-dashboard/balance-sheet/erc20_current_balances.sql
+++ dune-dashboards/treasury-dashboard/balance-sheet/erc20_current_balances.sql
@@ -31,7 +31,11 @@ UNION ALL
 
     SELECT 
         --evt_block_time,    
-        'Index Token Treasury' AS "wallet",
+        CASE 
+            WHEN address = '\x9467cfadc9de245010df95ec6a585a506a8ad5fc' THEN 'Index Treasury' 
+            WHEN address = '\xe2250424378b6a6dC912f5714cfd308a8D593986' THEN 'Index Treasury Committee'
+            WHEN address = '\x26e316f5b3819264DF013Ccf47989Fb8C891b088' THEN 'Community Treasury Year 1 Vesting'
+            END AS "wallet",
         address as wallet_address, 
         contract_address,
         sum(amount)/1e18 AS "units"
@@ -39,7 +43,11 @@ UNION ALL
     FROM transfers 
     
     GROUP BY 
-        --evt_block_time,
+        CASE 
+            WHEN address = '\x9467cfadc9de245010df95ec6a585a506a8ad5fc' THEN 'Index Treasury' 
+            WHEN address = '\xe2250424378b6a6dC912f5714cfd308a8D593986' THEN 'Index Treasury Committee'
+            WHEN address = '\x26e316f5b3819264DF013Ccf47989Fb8C891b088' THEN 'Community Treasury Year 1 Vesting'
+            END,
         address,
         contract_address
 )
@@ -70,22 +78,35 @@ UNION ALL
 
 )
 
-
+/* --- Main Query --- */
 SELECT 
-    wallet,
-    wallet_address,
-    symbol, 
+    tats.wallet,
+    tats.wallet_address,
+    mrp.symbol, 
     mrp.contract_address, 
-    units, 
-    price, 
-    (units * price) as balance
+    tats.units, 
+    mrp.price, 
+    (tats.units * mrp.price) as balance
     
 
 FROM transfer_amounts tats
 
 LEFT JOIN most_recent_prices mrp ON mrp.contract_address = tats.contract_address
 
-WHERE wallet_address = '\x9467cfadc9de245010df95ec6a585a506a8ad5fc'
-
-
-
+WHERE wallet_address IN ('\x9467cfadc9de245010df95ec6a585a506a8ad5fc', -- Treasury Wallet
+                         '\xe2250424378b6a6dC912f5714cfd308a8D593986', -- Treasury Committee Wallet
+                         '\x26e316f5b3819264DF013Ccf47989Fb8C891b088' -- Community Treasury Year 1 Vesting
+                         )
+                         
+GROUP BY 
+    tats.wallet,
+    tats.wallet_address,
+    mrp.symbol, 
+    mrp.contract_address, 
+    tats.units, 
+    mrp.price, 
+    (tats.units * mrp.price) 
+    
+ORDER BY 
+    tats.wallet,
+    (tats.units * mrp.price) DESC
